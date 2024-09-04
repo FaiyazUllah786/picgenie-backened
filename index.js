867dffe3e373ad688c0c36bc5fbf1fb6e9c1b73d
FAIYAZ ULLAH <faiyazullah54@gmail.com>
2024-09-04 21:32:05 +0530

    changing port

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ app.get("/login", (req, res) => {
   res.send("<h2>Please log in the app<h2/>");
 });
 
-app.listen(process.env.PORT, () =>
-  console.log("Server is running on PORT " + process.env.PORT)
-);
+app.get("/github", async (req, res) => {
+  await fetch("https://api.github.com/users/FaiyazUllah786")
+    .then((response) =>
+      response.json().then((data) => {
+        res.json(data);
+      })
+    )
+    .catch((err) => {
+      console.log("Error Occured", err.status);
+      res.json(err);
+    })
+    .finally(() => res.json({ message: "No Data Found" }));
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Server is running on PORT " + PORT));
